70cca458ccdf5008c12790564da0b4075f0fd29e
Tiago Siqueira Dionizio <tiagosdionizio@gmail.com>
2026-02-19 22:20:21 -0300

    Create Demonstrable.java

---
+++ src/main/java/com/patterns/common/Demonstrable.java
@@ -0,0 +1,9 @@
+package com.patterns.common;
+
+/**
+ * Common interface for all pattern demonstrations.
+ * Follows Interface Segregation Principle.
+ */
+public interface Demonstrable {
+    void demonstrate();
+}
